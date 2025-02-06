9807d8694f41b6fa276752867135dc9610181a8a
jameel213 <shalashjameel@gmail.com>
2025-02-06 14:18:37 +1100

    finsh

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ dice_type = 2
 def select_face_option(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-
+    dice_type = int(event.target.value)
 
 def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-    print(dice.dice_roll(6))
+    result = dice.dice_roll(dice_type)
+    document.querySelector("div#roll-history").innerHTML = "<p id='something'>" + str(result) + "</p>"
+
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
